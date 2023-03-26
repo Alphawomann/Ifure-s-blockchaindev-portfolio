32243ec123ae4855d9e2272c2a8cd62191f8f0bd
Alphawomann <ifureiwuo@gmail.com>
2023-03-26 23:13:28 +0100

    new updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ function Projects() {
       <div id="projects">
   <h2>Projects</h2>
   <p className="intro">My curiosity about the blockchain technology led me to invest time and resources to gain adept knowledge and practice on blockchain projects. These are some of  my excellent projects with links to the sourcecode on Github. Click the links to view them.</p>
-  <div className="boxcontainer"> 
+ <div className="boxcontainer"> 
     <div className="box"> 
 <img src={nft} alt="An NFT logo"/>
 < a href="https://github.com/Alphawomann/NFT.git">Github link to NFT source code</a> 
@@ -16,7 +16,25 @@ function Projects() {
 
     <div className="box"> 
 <img src={ERC20} alt="An ERC20 picture"/>
-< a href="https://github.com/Alphawomann/ERC20-">Github link to ERC20 source code</a> 
+< a href="https://github.com/Alphawomann/ERC20">Github link to ERC20 source code</a> 
+    </div> 
+
+    <div className="box"> 
+<img src={crowd} alt="This is a crowdfunding image"/>
+< a href="https://github.com/Alphawomann/crowdfunding-">Github link to crowdfunding source code</a> 
+    </div> 
+
+  </div> 
+
+  <div className="boxcontainer"> 
+    <div className="box"> 
+<img src={nft} alt="A staking image"/>
+< a href="https://github.com/Alphawomann/NFT.git">Github link to staking code</a> 
+    </div> 
+
+    <div className="box"> 
+<img src={ERC20} alt="A piggybank"/>
+< a href="https://github.com/Alphawomann/ERC20">Github link to piggybank code</a> 
     </div> 
 
     <div className="box"> 
@@ -25,6 +43,10 @@ function Projects() {
     </div> 
 
   </div>  
+
+
+
+
       </div> 
 
       
